843262f2eaf489c40dabe31b53d4c3c2ba4530da
idunnoguesswho <51185866+idunnoguesswho@users.noreply.github.com>
2026-04-11 18:39:36 -0600

    Update config.php

--- config.php
+++ config.php
@@ -2,4 +2,4 @@
   define('DB_HOST', 'sql103.infinityfree.com');
   define('DB_USER', 'if0_41606606');
   define('DB_PASS', 'Q4Y1cAGsEYCkgnj');
-  define('DB_NAME', 'stailian');
+  define('DB_NAME', 'if0_41606606_stailian');
