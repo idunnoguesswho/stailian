9ffb484d3fb922a995429cd6be491d84c6d621d7
idunnoguesswho <51185866+idunnoguesswho@users.noreply.github.com>
2026-04-11 20:10:47 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
-<? php
-  define('DB_HOST', 'sql103.infinityfree.com');
-  define('DB_USER', 'if0_41606606');
-  define('DB_PASS', 'Q4Y1cAGsEYCkgnj');
-  define('DB_NAME', 'if0_41606606_stailian');
+<?php
+define('DB_HOST', 'sql103.infinityfree.com');
+define('DB_USER', 'if0_41606606');
+define('DB_PASS', 'Q4Y1cAGsEYCkgnj');
+define('DB_NAME', 'if0_41606606_stailian');
